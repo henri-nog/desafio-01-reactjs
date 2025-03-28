70808029b36e27af49a811202f43195916a3043e
henri-nog <fellipe.caluete@a.ficr.edu.br>
2025-03-28 10:13:43 -0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const ano = data_atual.getFullYear();
 
 ReactDOM.createRoot(document.getElementById('root')).render (
    <div>
-    <h1>Olá, {nome} {sobrenome}</h1>
+    <h1>Olá! {nome} {sobrenome}</h1>
     <h2>{`Olá, ${nome} ${sobrenome}`}</h2>
     <h3>Olá, {nome + " " + sobrenome}</h3>
     <p>Seu número da sorte é {num} e o ano atual é {ano}</p>
